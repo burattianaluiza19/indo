7060afc4ead6fe1c7518ad8f8027f59203f07a30
burattianaluiza19 <ana.buratti.barbosa@escola.pr.gov.br>
2025-09-04 11:52:40 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 function anoca(){
     const variave = document.getElementById('nome_usuario').value;
     
-    document.getElementById('flash').innerHTML= variave ;
+    document.getElementById('ana').innerHTML= variave ;
 
 
 }
-    
+function anoco(){
+    const variave = document.getElementById('cpf_usuario').value;
+
+    document.getElementById('uu').innerHTML= variave;
+}    
